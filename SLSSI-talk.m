82675e7c3809d36c91ae4b32370b52666d250c74
Bryan Blette <bletteb18@gmail.com>
2019-02-12 10:32:02 -0500

    Update and rename _talks/2012-03-01-talk-1.md to SLSSI-talk.m

---
+++ SLSSI-talk.m
@@ -0,0 +1,8 @@
+---
+title: "Data Analysis Challenges in the Space Life Sciences"
+collection: talks
+type: "Presentation"
+venue: "Space Life Sciences Summer Institute Research Conference, Johnson Space Center"
+date: 2015-08-07
+location: "Houston, Texas"
+---
